7a0cedac4b5e8ce97fba7479ec0865bb508a4646
Sabith <masterdevsabith@gmail.com>
2025-08-29 22:20:33 +0530

    test timer worked

--- extension.js
+++ extension.js
@@ -1,36 +1,124 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  const productivity_methods = [
+    {
+      id: "pomodoro",
+      title: "Pomodoro",
+      description: "25 min focus + 5 min break",
+      work: 25,
+      break: 5,
+    },
+    {
+      id: "flowtime",
+      title: "Flowtime",
+      description: "Custom session length + flexible break",
+      work: 50,
+      break: 10,
+    },
+    {
+      id: "rule5217",
+      title: "52/17 Rule",
+      description: "52 min work + 17 min break",
+      work: 52,
+      break: 17,
+    },
+    {
+      id: "ultradian",
+      title: "Ultradian Rhythm",
+      description: "90 min deep work + 20 min rest",
+      work: 90,
+      break: 20,
+    },
+    {
+      id: "test-timer",
+      title: "Test Timer",
+      description: "Test timer with 1 min work + 1 min break",
+      work: 1,
+      break: 1,
+    },
+    {
+      id: "custom",
+      title: "Custom Timer",
+      description: "Set your own work and break durations",
+      work: null,
+      break: null,
+    },
+  ];
+  const mapped_productivity_method = productivity_methods.map((method) => {
+    return {
+      label: method.title,
+      detail: method.description,
+      method: method,
+    };
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "seta-timer" is now active!');
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('seta-timer.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  //intialising status bar
+  statusBarItem.command = "seta-timer.SetaProductivityTimer";
+  context.subscriptions.push(statusBarItem);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from seta-timer!');
-	});
+  //testing status bar
+  //   statusBarItem.text = "$(zap) Testing Seta-Timer";
+  //   statusBarItem.show();
 
-	context.subscriptions.push(disposable);
+  const disposable = vscode.commands.registerCommand(
+    "seta-timer.SetaProductivityTimer",
+    async function () {
+      const method = await vscode.window.showQuickPick(
+        mapped_productivity_method,
+        {
+          matchOnDetail: true,
+          placeHolder: "Choose a productivity method",
+        }
+      );
+
+      if (!method) return;
+
+      let work_minutes = method.method.work;
+      let break_minutes = method.method.break;
+      vscode.window.showInformationMessage("Hello World from seta-timer!");
+
+      startTimer(
+        work_minutes,
+        break_minutes,
+        method.method.title,
+        statusBarItem
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
+function startTimer(workmin, breakmin, title, statusBarItem) {
+  let seconds = workmin * 60;
+  statusBarItem.show();
+  console.log(`${title} technique has started, with ${workmin} minutes`);
+
+  const interval = setInterval(() => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    statusBarItem.text = `$(clock) ${title}: ${mins}m ${secs}s`;
+
+    if (seconds <= 0) {
+      clearInterval(interval);
+      statusBarItem.text = `$(clock) ${title} Done!`;
+    }
+    seconds--;
+  }, 1000);
+}
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
